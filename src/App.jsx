7d76c821a6b3c6f9118bea9473999db29b0dc4a4
AndreAleman <davidandrealeman@gmail.com>
2024-02-27 08:48:51 -0500

    add google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import HomePage from "./pages/HomePage.jsx";
-
+import ReactGA from 'react-ga';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
+ReactGA.initialize('G-V8WGGBQ7FN');
 function App() {
   return (
     <>
