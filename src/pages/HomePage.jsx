16de7d7e5e2d07e3ab8cc773c72d738021098da4
AndreAleman <davidandrealeman@gmail.com>
2024-02-27 12:34:14 -0500

    page view

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,8 +5,11 @@ import Services from "@/components/Services/Services.jsx";
 import Portfolio from "@/components/Portfolio/Portfolio.jsx";
 import Contacts from "@/components/Contacts/Contacts.jsx";
 import Footer from "@/components/Footer/Footer.jsx";
+import { useContext, useEffect } from "react";
+import ReactGA from 'react-ga';
 
 const HomePage = () => {
+  ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <>
       <div className="container">
