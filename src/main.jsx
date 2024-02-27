b032357dbb361a2345f83ec823aa8b3dc092e02b
AndreAleman <davidandrealeman@gmail.com>
2024-02-27 12:50:05 -0500

    please

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 import ReactGA from 'react-ga';
 
 
-ReactGA.initialize('G-V8WGGBQ7FN');
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
